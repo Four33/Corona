db031637dbcc00c2856d080d137fbfbc74479583
Gerrit Versteeg <gversteeg@fourpoints.nl>
2020-06-04 14:49:34 +0200

    first test

--- Get_ECDC_Data.R
+++ Get_ECDC_Data.R
@@ -1,17 +1,19 @@
 # To retreive data from European Centre for Disease Prevention and Control
-# source:
-# https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
+# source: https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
 
+## housekeeping
 library(utils)
 library(httr)
+library(tidyverse)
 
-#download the dataset from the ECDC website to a local temporary file
+# download the dataset from ECDC to a local temporary file and copy into DF data
 GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", 
     authenticate(":", ":", type="ntlm"), 
     write_disk(tf <- tempfile(fileext = ".csv")))
-
-#read the Dataset sheet into “R”. The dataset will be called "data".
 data <- read.csv(tf)
 
-
+## clean dataset
+df_cln <- data %>%
+  mutate(Rpt_date = as.Date(as.character(dateRep), "%d/%m/%Y")) %>% 
+  mutate(Cont_rate = cases / popData2018 * 100)
 
