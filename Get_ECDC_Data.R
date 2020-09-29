0f76ec9aa820ddefecf5c1a92c110014eaed478e
Gerrit Versteeg <gversteeg@docstore.immd1-ds.nl>
2020-09-29 17:17:34 +0200

    Plotje toevoegen

--- Get_ECDC_Data.R
+++ Get_ECDC_Data.R
@@ -18,3 +18,10 @@ df_cln <- data %>%
   mutate(Cont_rate = cases / popData2019 * 100)
 
 
+## plotting the data
+df_cln %>% 
+  filter(countriesAndTerritories == "Netherlands") %>% 
+  ggplot(aes(Rpt_date, Cont_rate)) +
+  geom_col() +
+  scale_y_continuous(labels = scales::percent_format())
+
